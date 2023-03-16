b09bbbe8ac0470b4edbfe4b1660014004874d6b1
Ciaran Huang-Ryan <85781351+ciaranhr@users.noreply.github.com>
2023-03-17 00:00:10 +1000

    Trivia Class structures

--- lib/bot/TriviaGame.py
+++ lib/bot/TriviaGame.py
@@ -1,7 +1,8 @@
 import requests
 import mysql.connector
 from mysql.connector import errorcode
-
+import html
+import db
 
 trivia_url_base = 'https://opentdb.com/api.php?'
 
@@ -14,31 +15,59 @@ def retrieve_trivia(num, difficulty):
         print("Bad Request")
      
     if trivia_response["response_code"] == 0:
-        return trivia_response
+        unescaped = []
+        for dict in trivia_response['results']:
+            for k in dict:
+                k = html.unescape(k)
+            unescaped.append(dict)        
+        return unescaped
     else:
         return trivia_response["response_code"]
+
+
+class Question():
+    def __init__(self, question, answers) -> None:
+        self.question = question
+        self.answers = answers
+        self.correct_a = answers[1]
+        self.incorrect_a = answers[0]
+
+    def combine_q_a(self):
+        pass
 
 
 class Trivia():
     def __init__(self, trivia_json) -> None:
         self.trivia_json = trivia_json
-        self.trivia
+        self.difficulty = self.make_difficulty()
+        self.questions = self.get_questions()
+        self.scores = self.make_scores()
 
     def get_questions(self):
         """retrieve questions """
-        qs_list = {i['question']:[i['incorrect_answers'], i['correct_answer']] for i in self.trivia_json['results']}
-        return qs_list
+        qs_dict = {i['question']:[i['incorrect_answers'], i['correct_answer']] for i in self.trivia_json}
+        return qs_dict
 
-    def get_difficulty(self):
-        difficulty = {i['question']:i['difficulty'] for i in self.trivia_json['results']}
+    def make_difficulty(self):
+        difficulty = {i['question']:i['difficulty'] for i in self.trivia_json}
         conversion = {'easy': 1, 'medium': 2, 'hard': 3}
         
         for k, v in difficulty.items():
             difficulty[k] = conversion[v]
         return difficulty
+    
+    def get_difficulty(self):
+        return self.difficulty
+    
+    def get_scores(self):
+        return self.scores
+
+    def make_scores(self):
+        scores = {k:v*2 for (k, v) in self.difficulty.items()}
+        return scores
 
     def input_questions():
-
+        stmt = "INSERT INTO questions ()"
         pass
 
     def get_scores():
