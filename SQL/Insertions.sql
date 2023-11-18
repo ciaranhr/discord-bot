6d2dec7e1cae538360ced8f03a5fc314583adb80
Ciaran Huang-Ryan <85781351+ciaranhr@users.noreply.github.com>
2023-11-18 23:57:08 +1000

    Updated Trivia Queries

---
+++ SQL/Insertions.sql
@@ -0,0 +1,8 @@
+/*
+INSERT INTO discordinfo.questions (type, level, trivia_score, content)
+	VALUES ('boolean', 1, 3, 'A group of islands is called an ''archipelago''');
+*/
+    
+INSERT INTO discordinfo.questions (type, level, trivia_score, content)
+	VALUES ('multiple', 3, 3, 'What year was the first San Diego Comic-Con?'
+);
